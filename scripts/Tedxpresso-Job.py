95aa46ee04dc8d5fa2c58b03a2c8ef42291e2be0
ZanottiMatteo <m.zanotti13@studenti.unibg.it>
2025-05-09 16:11:23 +0200

    Update Tedxpresso-Job.py

--- scripts/Tedxpresso-Job.py
+++ scripts/Tedxpresso-Job.py
@@ -1,109 +1,159 @@
-###### TEDx-Load-Aggregate-Model
-######
-
 import sys
 import json
 import pyspark
-from pyspark.sql.functions import col, collect_list, array_join
-
+from pyspark.sql.functions import col, collect_list, explode, struct
 from awsglue.transforms import *
 from awsglue.utils import getResolvedOptions
 from pyspark.context import SparkContext
 from awsglue.context import GlueContext
 from awsglue.job import Job
+from awsglue.dynamicframe import DynamicFrame
 
-
-
-
-##### FROM FILES
-tedx_dataset_path = "s3://tedxpresso-data-mp/final_list.csv"
-
-###### READ PARAMETERS
+# ----- READ PARAMETERS -----
 args = getResolvedOptions(sys.argv, ['JOB_NAME'])
-
-##### START JOB CONTEXT AND JOB
 sc = SparkContext()
-
-
 glueContext = GlueContext(sc)
 spark = glueContext.spark_session
-
-
-    
 job = Job(glueContext)
 job.init(args['JOB_NAME'], args)
 
-
-#### READ INPUT FILES TO CREATE AN INPUT DATASET
+# ----- LOAD MAIN TEDX DATASET -----
+tedx_dataset_path = "s3://tedxpresso-data-mp/final_list.csv"
 tedx_dataset = spark.read \
     .option("header","true") \
     .option("quote", "\"") \
     .option("escape", "\"") \
     .csv(tedx_dataset_path)
-    
-tedx_dataset.printSchema()
 
-
-#### FILTER ITEMS WITH NULL POSTING KEY
+# FILTER ITEMS WITH NULL POSTING KEY
 count_items = tedx_dataset.count()
 count_items_null = tedx_dataset.filter("id is not null").count()
-
 print(f"Number of items from RAW DATA {count_items}")
 print(f"Number of items from RAW DATA with NOT NULL KEY {count_items_null}")
 
-## READ THE DETAILS
+# ----- LOAD DETAILS -----
 details_dataset_path = "s3://tedxpresso-data-mp/details.csv"
 details_dataset = spark.read \
     .option("header","true") \
     .option("quote", "\"") \
     .option("escape", "\"") \
-    .csv(details_dataset_path)
+    .csv(details_dataset_path) \
+    .withColumnRenamed("interalId", "internal_id")
 
-details_dataset = details_dataset.select(col("id").alias("id_ref"),
-                                         col("description"),
-                                         col("duration"),
-                                         col("publishedAt"))
+details_dataset = details_dataset.select(
+    col("id").alias("id_ref"),
+    col("description"),
+    col("duration"),
+    col("publishedAt"),
+    col("internal_id")
+)
 
-# AND JOIN WITH THE MAIN TABLE
-tedx_dataset_main = tedx_dataset.join(details_dataset, tedx_dataset.id == details_dataset.id_ref, "left") \
-    .drop("id_ref")
+# ----- JOIN DETAILS TO MAIN TEDX -----
+tedx_dataset_main = tedx_dataset.join(
+    details_dataset,
+    tedx_dataset.id == details_dataset.id_ref,
+    "left"
+).drop("id_ref")
 
-tedx_dataset_main.printSchema()
-
-## READ TAGS DATASET
+# ----- LOAD TAGS AND AGGREGATE -----
 tags_dataset_path = "s3://tedxpresso-data-mp/tags.csv"
 tags_dataset = spark.read.option("header","true").csv(tags_dataset_path)
+tags_dataset_agg = tags_dataset.groupBy(
+    col("id").alias("id_ref")
+).agg(
+    collect_list("tag").alias("tags")
+)
+tedx_dataset_agg = tedx_dataset_main.join(
+    tags_dataset_agg,
+    tedx_dataset_main.id == tags_dataset_agg.id_ref,
+    "left"
+).drop("id_ref")
 
+# ----- LOAD IMAGES + ENRICH DETAILS -----
+images_dataset = spark.read.option("header", "true").csv("s3://tedxpresso-data-mp/images.csv") \
+    .withColumnRenamed("url", "image_url")
 
-CREATE THE AGGREGATE MODEL, ADD TAGS TO TEDX_DATASET
-tags_dataset_agg = tags_dataset.groupBy(col("id").alias("id_ref")).agg(collect_list("tag").alias("tags"))
-tags_dataset_agg.printSchema()
-tedx_dataset_agg = tedx_dataset_main.join(tags_dataset_agg, tedx_dataset.id == tags_dataset_agg.id_ref, "left") \
-    .drop("id_ref")
+final_list_extra = spark.read.option("header", "true").csv("s3://tedxpresso-data-mp/final_list.csv") \
+    .withColumnRenamed("url", "video_url") \
+    .withColumnRenamed("speakers", "speaker") \
+    .select("id", "title", "speaker", "video_url")
 
-tedx_dataset_agg.printSchema()
+details_full = details_dataset.join(
+    images_dataset,
+    details_dataset.id_ref == images_dataset.id,
+    "left"
+).drop(images_dataset.id).join(
+    final_list_extra,
+    details_dataset.id_ref == final_list_extra.id,
+    "left"
+).drop(final_list_extra.id)
 
-#READ RELATED_VIDEOS DATASET
+# ----- LOAD RELATED VIDEOS LIST -----
 related_videos_dataset_path = "s3://tedxpresso-data-mp/related_videos.csv"
 related_videos_dataset = spark.read.option("header","true").csv(related_videos_dataset_path)
+related_videos_dataset_agg = related_videos_dataset.groupBy(
+    col("id").alias("id_ref")
+).agg(
+    collect_list("related_id").alias("related_ids")
+)
 
-#CREATE THE AGGREGATE MODEL, ADD RELATED_VIDEOS TO TEDX_DATASET
-related_videos_dataset_agg = related_videos_dataset.groupBy(col("id").alias("id_ref")).agg(collect_list("related_id").alias("related_ids"))
-related_videos_dataset_agg.printSchema()
-tedx_dataset_agg1 = tedx_dataset_agg.join(related_videos_dataset_agg, tedx_dataset_agg.id == related_videos_dataset_agg.id_ref, "left") \
-    .drop("id_ref")
+# ----- EXPLODE RELATED IDS TO LOOKUP FULL DETAILS -----
+exploded = related_videos_dataset_agg.select(
+    col("id_ref").alias("main_id"),
+    explode("related_ids").alias("related_internal_id")
+)
 
-tedx_dataset_agg1.printSchema()
+related_with_details = exploded.join(
+    details_full,
+    exploded.related_internal_id == details_full.internal_id,
+    "left"
+).filter(
+    col("internal_id").isNotNull()
+)
 
-tedx_dataset_final = tedx_dataset_agg1.withColumnRenamed("id", "_id")
+# ----- DEDUPLICATE RELATED VIDEOS per main_id + internal_id -----
+related_with_details_unique = related_with_details.dropDuplicates([
+    "main_id", "internal_id"
+])
 
+# ----- AGGREGATE DEDUPLICATED RELATED DETAILS -----
+related_aggregated = related_with_details_unique.groupBy("main_id").agg(
+    collect_list(
+        struct(
+            col("id_ref").alias("id"),
+            col("internal_id"),
+            col("title"),
+            col("speaker"),
+            col("video_url"),
+            col("description"),
+            col("duration"),
+            col("publishedAt"),
+            col("image_url")
+        )
+    ).alias("related_videos_details")
+)
+
+# ----- JOIN FINAL TEDX DATASET WITH RELATED STRUCT -----
+tedx_dataset_final = tedx_dataset_agg.join(
+    related_aggregated,
+    tedx_dataset_agg.id == related_aggregated.main_id,
+    "left"
+).drop("main_id").withColumnRenamed("id", "_id")
+
+# ----- SAVE TO MONGODB -----
 write_mongo_options = {
     "connectionName": "Mongodbatlas connection",
     "database": "unibg_tedx_2025",
     "collection": "tedx_data",
     "ssl": "true",
-    "ssl.domain_match": "false"}
-from awsglue.dynamicframe import DynamicFrame
-tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_final, glueContext, "nested")
+    "ssl.domain_match": "false"
+}
 
-glueContext.write_dynamic_frame.from_options(tedx_dataset_dynamic_frame, connection_type="mongodb", connection_options=write_mongo_options)
+tedx_dataset_dynamic_frame = DynamicFrame.fromDF(tedx_dataset_final, glueContext, "nested")
+glueContext.write_dynamic_frame.from_options(
+    tedx_dataset_dynamic_frame,
+    connection_type="mongodb",
+    connection_options=write_mongo_options
+)
+
+job.commit()
